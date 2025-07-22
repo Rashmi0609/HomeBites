b808ad2a1c3989b26386ea4c4a4f739e1e9efcba
Rashmi0609 <rashmi63867@gmail.com>
2025-07-22 23:53:47 +0530

    Update server.cjs and add package files

--- homebites-react-app/Backend/server.cjs
+++ homebites-react-app/Backend/server.cjs
@@ -55,6 +55,8 @@ app.get('/logout', (req, res) => {
   });
 });
 
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
